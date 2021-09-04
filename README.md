308c5dfbe36ae2f592f83bf042af8062e6bef83a
alexnesov <alexandrenesovic@gmail.com>
2021-09-04 12:58:33 +0200

    working db manage

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Indicators-java
+
+mysql connector in: /usr/share/java/mysql/ysql-connector-java-8.0.25.jar</br>
+
+**Compile the java file:**</br>
+mkdir out</br>
+javac -d out src/Main.java </br>
+
+
+**Run the java program:**</br>
+java -cp .:../lib/mysql-connector-java-8.0.25.jar Main
+
+
+</br>
+
+Choosing the desired java version:</br>
+sudo update-alternatives --config java
+
+
+
--- src/com/util/db_manage.java
+++ src/com/util/db_manage.java
@@ -4,20 +4,18 @@ import java.util.Properties;
 
 public class db_manage {
 
-
     public void testing() {
         System.out.println("DB Manage called. ");
     }
 
     private static final String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DATABASE_URL = System.getenv("aws_db_endpoint");
+    private static final String DATABASE_URL = System.getenv("aws_db_endpoint_java");
     private static final String USERNAME = System.getenv("aws_db_user");
     private static final String PASSWORD = System.getenv("aws_db_pass");
 
     // init connection object
     private Connection connection;
 
-    
     // connect database
     public Connection connect() {
         if (connection == null) {
@@ -44,53 +42,3 @@ public class db_manage {
 
     }
 }
-    /*
-    public static void test(){
-
-        System.out.println("MySQL connect example.");
-        String formattedURL = "jdbc:mysql://" + DATABASE_URL;
-
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-        // 1. Get a connection to database
-        Class.forName(DATABASE_DRIVER);
-        Connection connection = DriverManager.getConnection(formattedURL, USERNAME, PASSWORD); 
-        // 2. Create a statement
-
-        stmt = connection.createStatement();
-        System.out.println("Connected!");
-        // 3. Execute SQL query
-        rs = stmt.executeQuery("SELECT * FROM marketdata.sp500");
-
-        // 4. Process the result set
-        ResultSetMetaData rsmd = rs.getMetaData();
-
-        // 4.a Get Number of cols, needed for .getColumnName() argument
-        int nbCols = rsmd.getColumnCount();
-
-        // 4b. Get the column names
-        for (int i = 1; i <= nbCols; i++) {
-            System.out.println(rsmd.getColumnName(i));
-        }
-
-        // 4c. Printing the values
-        while (rs.next()) {
-            for (int i = 1; i <= nbCols; i++) {
-                if (i > 1) System.out.print(",  ");
-                String columnValue = rs.getString(i);
-                System.out.print(columnValue + " " + rsmd.getColumnName(i));
-            }
-            System.out.println("");
-        }
-        } catch(ClassNotFoundException | SQLException e)
-        {
-            System.err.println("SQL exception: " + e.getMessage());
-            System.exit(1);
-        }
-    */
-       
- 
-
-
